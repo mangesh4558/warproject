b6f7e0dd996414fd7f71c6235eb1abb17fb79a08
mangesh4558 <mangeshabnave@yahoo.com>
2018-09-20 16:12:08 +0530

    Update HelloServlet.java

--- src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
+++ src/main/java/com/boxfuse/samples/javawarhello/HelloServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet(urlPatterns = "/hello", loadOnStartup = 1)
+@WebServlet(urlPatterns = "/hello1", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
